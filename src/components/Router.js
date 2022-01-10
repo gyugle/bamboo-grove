f431d89483010548dba8696da84f84ec880a6a8c
gyugle <gyugle.9x@gmail.com>
2022-01-10 18:39:24 +0900

    Add Profile route.

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,17 +4,18 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
 import SignUp from '../routes/SignUp';
-const AppRouter = ({ isUser }) => {
+import Profile from '../routes/Profile';
+const AppRouter = ({ isUser, userInfo }) => {
   return (
     <div>
       <Router>
         <Routes>
           {isUser ? (
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home userInfo={userInfo} />} />
           ) : (
             <Route path="/" element={<Auth />} />
           )}
-
+          <Route path="/profile" element={<Profile userInfo={userInfo} />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
