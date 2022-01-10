da88d2b677a0c8f59c145d874bdad8432b01cf40
gyugle <gyugle.9x@gmail.com>
2022-01-10 18:38:29 +0900

    Add components(Posting, ShowPostings) and route(Profile).

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
+import Posting from '../components/Posting';
+import ShowPostings from '../components/ShowPostings';
 
-function Home() {
+function Home({ userInfo }) {
   const navigate = useNavigate();
+
   const logOut = async () => {
     await signOut(auth);
     navigate('/');
   };
+
   return (
     <div>
-      <h3>Welcome HOME</h3>
+      <h3>Welcome to Bamboo Grove!</h3>
+      <Link to="/profile">
+        <button>Edit Profile</button>
+      </Link>
       <button onClick={logOut}>LogOut</button>
+
+      <Posting userInfo={userInfo} />
+
+      <hr />
+      <ShowPostings />
+      <hr />
     </div>
   );
 }
