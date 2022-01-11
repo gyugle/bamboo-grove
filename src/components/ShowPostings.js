ee47815ccf4bd58a38453a8faefe3034fd207061
gyugle <gyugle.9x@gmail.com>
2022-01-11 15:18:25 +0900

    Add function that arranging postings and add info.

--- src/components/ShowPostings.js
+++ src/components/ShowPostings.js
@@ -1,27 +1,89 @@
 import { useState, useEffect } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
-import { db } from '../firebase';
-function ShowPostings() {
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { auth, db } from '../firebase';
+import Posting from './Posting';
+import UpdateModal from './UpdateModal';
+
+function ShowPostings({ userInfo }) {
+  const [openModal, setOpenModal] = useState(false);
+  const [select, setSelect] = useState('');
+
   const [postings, setPostings] = useState([]);
+
   useEffect(() => {
-    onSnapshot(collection(db, 'postings'), (snapshot) => {
-      setPostings(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }))
-      );
-      postings.sort((a, b) => b[3] - a[3]);
-    });
+    onSnapshot(
+      query(collection(db, 'postings'), orderBy('createdAt', 'desc')),
+      (snapshot) => {
+        setPostings(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        );
+      }
+    );
   }, []);
 
-  postings.map((posting) => console.log(posting.createAt));
+  const convertTime = (msTime) => {
+    const timeStamp = msTime;
+    const date = new Date(timeStamp);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
+      2,
+      '0'
+    )}-${String(date.getDate()).padStart(2, '0')} ${String(
+      date.getHours()
+    ).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+  };
+
+  const onClickDelete = async (id, event) => {
+    const ok = window.confirm('Are you sure to delete?');
+    if (ok) {
+      await deleteDoc(doc(db, 'postings', id));
+    }
+  };
+
+  const onClickOpenModal = (posting, event) => {
+    setSelect(posting.id);
+
+    setOpenModal(true);
+  };
   return (
     <div>
       <div>
         {postings.map((posting) => (
           <div key={posting.id}>
-            {posting.text} {posting.createAt}
+            <h6>{posting.name}</h6>
+            <h6>{posting.createdBy}</h6>
+            <h4>{posting.text}</h4>
+            <span>Create at {convertTime(posting.createdAt)}</span>
+            {posting.updatedAt && (
+              <div>Update at {convertTime(posting.updatedAt)}</div>
+            )}
+
+            <button onClick={(event) => onClickOpenModal(posting, event)}>
+              Edit
+            </button>
+            {openModal && select === posting.id && (
+              <UpdateModal
+                setOpenModal={setOpenModal}
+                setSelect={setSelect}
+                posting={posting}
+              />
+            )}
+            <button
+              onClick={(event) => {
+                onClickDelete(posting.id, event);
+              }}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
