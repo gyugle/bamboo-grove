5d27fe5c1aeaf5758d0562812b81249f1fb18cca
gyugle <gyugle.9x@gmail.com>
2022-01-13 14:24:29 +0900

    Initialize navbar component.

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li>HOME</li>
+        <li>PROFILE</li>
+        <li>LOGOUT</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
