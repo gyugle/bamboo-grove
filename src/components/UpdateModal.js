635eecaa5ba60d35947768d6fd51b927dbb4aedc
gyugle <gyugle.9x@gmail.com>
2022-01-13 14:32:05 +0900

    Fix initial value.

--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { db } from '../firebase';
 
 function UpdateModal({ setOpenModal, setSelect, posting }) {
-  const [newPost, setNewPost] = useState('');
+  const [newPost, setNewPost] = useState(posting.text);
   const onChange = (event) => {
     setNewPost(event.target.value);
   };
@@ -22,7 +22,7 @@ function UpdateModal({ setOpenModal, setSelect, posting }) {
       <h3>Modal Updating...</h3>
       <div>
         <form onSubmit={onSubmitUpdate}>
-          <input onChange={onChange} />
+          <input onChange={onChange} value={newPost} />
 
           <button>UPDATE</button>
         </form>
