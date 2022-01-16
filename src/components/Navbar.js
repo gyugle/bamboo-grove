86042464c85920cba85c134d69a8c38fbe5de2d5
gyugle <gyugle.9x@gmail.com>
2022-01-16 20:37:17 +0900

    Made Navbar component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,48 @@
+import homeIcon from '../icon/home.png';
+import mypostIcon from '../icon/mypost.png';
+import profileIcon from '../icon/profile.png';
+import logoutIcon from '../icon/logout.png';
+import styles from '../css/navbar.module.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from '../firebase';
+import { signOut } from 'firebase/auth';
+
 function Navbar() {
+  const navigate = useNavigate();
+  const logOut = async () => {
+    const ok = window.confirm('Are you sure?');
+    if (ok) {
+      await signOut(auth);
+      navigate('/');
+    }
+  };
   return (
-    <div>
-      <ul>
-        <li>HOME</li>
-        <li>PROFILE</li>
-        <li>LOGOUT</li>
-      </ul>
+    <div className={styles.main}>
+      <div className={styles.list}>
+        <div>
+          <Link to="/">
+            <img src={homeIcon} className={styles.icon} alt="home" />
+          </Link>
+        </div>
+        <div>
+          <Link to="/mypostings">
+            <img src={mypostIcon} className={styles.icon} alt="mypost" />
+          </Link>
+        </div>
+        <div>
+          <Link to="/profile">
+            <img src={profileIcon} className={styles.icon} alt="profile" />
+          </Link>
+        </div>
+        <div>
+          <img
+            src={logoutIcon}
+            className={styles.icon}
+            onClick={logOut}
+            alt="logout"
+          />
+        </div>
+      </div>
     </div>
   );
 }
